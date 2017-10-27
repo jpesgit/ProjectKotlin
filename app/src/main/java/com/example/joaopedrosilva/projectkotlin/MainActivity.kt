707ca71143227bc4c0d7563d7fe2fb7619a6d238
joaopedrosilva <jpesmail@gmail.com>
2017-10-27 14:59:36 +0100

    fix OnClickListener fix equals String

--- app/src/main/java/com/example/joaopedrosilva/projectkotlin/MainActivity.kt
+++ app/src/main/java/com/example/joaopedrosilva/projectkotlin/MainActivity.kt
@@ -1,31 +1,36 @@
 package com.example.joaopedrosilva.projectkotlin
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-    class MainActivity : AppCompatActivity() {
+class MainActivity :
+        AppCompatActivity(),
+        View.OnClickListener {
+    override fun onClick(v: View?) {
+        login(etUsername.text.toString(), etPassword.text.toString())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         welcomeMessage.setText(getString(R.string.app_name))
+        buttonLogin.setOnClickListener(this)
 
-        buttonLogin.setOnClickListener{
-            login(etUsername.text.toString(),etPassword.text.toString())
-        }
 
 
     }
-    fun login(user:String , pass:String){
-        if(user.equals("a")&& pass.equals("a")) {
+
+    fun login(user: String, pass: String) {
+        if (user == "a" && pass == "a") {
             Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
-            val intent = Intent(this@MainActivity, SecondActivity::class.java)
-            startActivity(intent)
-        }else{
+            startActivity(Intent(this, SecondActivity::class.java))
+        } else {
             Toast.makeText(this, "Try again", Toast.LENGTH_LONG).show()
         }
     }
