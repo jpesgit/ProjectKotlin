88913be681d59e69ae911f2e7933ce97ab53d3a7
joaopedrosilva <jpesmail@gmail.com>
2017-11-06 14:50:16 +0000

    finish list Wikipedia

--- app/src/main/java/com/example/joaopedrosilva/projectkotlin/main/WikiSearch/WikiAdapter.kt
+++ app/src/main/java/com/example/joaopedrosilva/projectkotlin/main/WikiSearch/WikiAdapter.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.example.joaopedrosilva.projectkotlin.R
+import kotlinx.android.synthetic.main.item_wiki.view.*
+import java.text.SimpleDateFormat
 
 /**
  * Created by joaopedrosilva on 06/11/17.
@@ -35,9 +37,15 @@ class WikiAdapter(val wikis: MutableList<Search>) : RecyclerView.Adapter<Recycle
     @Suppress("JoinDeclarationAndAssignment")
     class Item(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val title_tv: TextView
+        private val date_tv: TextView
+        private val page_id_tv: TextView
+        private val snippet_tv: TextView
 
         init {
             this.title_tv = itemView.findViewById(R.id.title_tv)
+            this.date_tv = itemView.findViewById(R.id.date_tv)
+            this.page_id_tv = itemView.findViewById(R.id.page_id_tv)
+            this.snippet_tv = itemView.findViewById(R.id.snippet_tv)
         }
 
         // vê o bytecode que esta classe gerou.
@@ -45,6 +53,18 @@ class WikiAdapter(val wikis: MutableList<Search>) : RecyclerView.Adapter<Recycle
         // (o código que é gerado é um findViewById, o que só é preciso fazer 1 vez)
         fun bindData(search: Search) = with(itemView) {
             title_tv.text = search.title
+            date_tv.text = formatDate(search.timestamp)
+            page_id_tv.text = "Page id: ${search.pageid}"
+            snippet_tv.text = search.snippet
+        }
+
+        fun formatDate(inputText: String): String {
+            val outputFormat = SimpleDateFormat("MM/yyyy")
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+
+            val date = inputFormat.parse(inputText)
+            val outputText = outputFormat.format(date)
+            return outputText
         }
     }
 }
